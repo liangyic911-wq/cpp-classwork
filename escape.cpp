c37b45d9e828029cf78334e850d856ae6387565c
liangyic911-wq <liangyic911@gmail.com>
2026-05-22 11:19:31 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+    cout << "1.豴豴豴豴" << endl; 
+    cout << "2.豴豴 豴豴" << endl; 
+    cout << "3.豴豴\t豴豴" << endl; 
+    cout << "4.豴豴\n豴豴" << endl;
+    cout << "\"5.豴豴豴豴\"" << endl; 
+    cout << "\'6.豴豴豴豴\'" << endl; 
+    cout << "\\7.豴豴豴豴\\" << endl;
+    return 0;
+}
